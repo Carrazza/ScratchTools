37c9ea6df1d6baa39e450dbe180a62e86db77a4f
Carrazza <brunocarrazza@gmail.com>
2022-11-17 11:59:07 -0500

    tudo

--- requester.sh
+++ requester.sh
@@ -4,9 +4,10 @@ while read line
 do
 	
 	sanitizedName=$(echo $line | unfurl paths | sed 's/\///')
+	
 
-	curl -Ls $line > $sanitizedName &
-	curl --head -Ls $line > $sanitizedName.header &
+	curl -Ls $line > "$sanitizedName"
+	curl --head -Ls $line > "$sanitizedName.header" 
 
 done
 
